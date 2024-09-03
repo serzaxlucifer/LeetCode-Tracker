4748d0ab151fc60fe3313ff5fd59798c77d608b8
Mukul Malik <mukulsinghmalik@gmail.com>
2024-09-03 23:31:55 +0530

    json fixes

--- server/index.js
+++ server/index.js
@@ -32,7 +32,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://leetcode-tracker.pages.dev',
+    credentials: true,
+}));
 
 connect(); 
 const checkForChanges = async () => 
