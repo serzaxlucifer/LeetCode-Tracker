72391fe785f3dc7489059a8ab16ebfc43858230e
Mukul Malik <mukulsinghmalik@gmail.com>
2024-09-03 23:01:40 +0530

    json fixes

--- server/routes/ping.js
+++ server/routes/ping.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/ping', (req, res) => {res.send(<p>System is working!</p>)});
+router.get('/ping', (req, res) => {res.send("<p>System is working!</p>")});
 
 module.exports = router;
