18dc1a9453ade47ca6a41003a00d74af844b45b3
Mukul Malik <mukulsinghmalik@gmail.com>
2024-09-03 16:06:57 +0530

    json fixes

---
+++ frontend/public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "BASE_URL": "https://leetcode-tracker-vavc.onrender.com"
+};
--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,26 +1,26 @@
 
-require("dotenv").config(); 
+const BASE_URL = import.meta.env.BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = 
 {
-  LOGIN_API: process.env.BASE_URL + "/auth/google"
+  LOGIN_API: BASE_URL + "/auth/google"
 };
 
 // PROFILE ENDPOINTS
 export const profileEndpoints = 
 {
-  GET_USER_DETAILS_API: process.env.BASE_URL + "/dashboard/get-profile",
-  UPDATE_USER_DETAILS_API: process.env.BASE_URL + "/dashboard/update-profile",
-  DELETE_USER_DETAILS_API: process.env.BASE_URL + "/dashboard/delete-profile",
-  DB_SWITCH_API: process.env.BASE_URL + "/dashboard/db-change"  
+  GET_USER_DETAILS_API: BASE_URL + "/dashboard/get-profile",
+  UPDATE_USER_DETAILS_API: BASE_URL + "/dashboard/update-profile",
+  DELETE_USER_DETAILS_API: BASE_URL + "/dashboard/delete-profile",
+  DB_SWITCH_API: BASE_URL + "/dashboard/db-change"  
 };
 
 
 // STATISTICS ENDPOINTS
 export const statisticsEndpoints = 
 {
-  GET_COMMUNITY_API: process.env.BASE_URL + "/dashboard/get-community",
-  GET_MY_STATS_API: process.env.BASE_URL + "/dashboard/get-submissions",
-  GET_MY_STATS_TOPIC_API: process.env.BASE_URL + "/dashboard/get-submissions-topic"
+  GET_COMMUNITY_API: BASE_URL + "/dashboard/get-community",
+  GET_MY_STATS_API: BASE_URL + "/dashboard/get-submissions",
+  GET_MY_STATS_TOPIC_API: BASE_URL + "/dashboard/get-submissions-topic"
 };
